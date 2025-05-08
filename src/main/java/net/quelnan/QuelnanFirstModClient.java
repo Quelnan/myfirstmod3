26bdd52c137bf38bd6c6bd7f85657f6a0526ea3b
Quelnan <egorkayyt101@gmail.com>
2025-05-08 22:58:44 +0800

    Initial commit

---
+++ src/main/java/net/quelnan/QuelnanFirstModClient.java
@@ -0,0 +1,10 @@
+package net.quelnan;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuelnanFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/quelnan/QuelnanFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.quelnan;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class QuelnanFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
